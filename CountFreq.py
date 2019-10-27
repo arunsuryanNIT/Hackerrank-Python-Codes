c47f6e3bb61259232b13079ade6fad82705a4f80
Gagan Deep Singh <gagangulyanig@gmail.com>
2019-10-27 13:50:59 +0530

    Improved Algo
    for searching for element in dictionary

--- CountFreq.py
+++ CountFreq.py
@@ -2,7 +2,7 @@ n = int(input())
 s = [int (i) for i in input().split()]
 freq = {} 
 for item in s: 
-    if (item in freq): 
+    if freq.get(item) != None: 
         freq[item] += 1
     else: 
         freq[item] = 1
